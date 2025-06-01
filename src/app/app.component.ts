b88048de536c3dd0cc8cd0ad35bc0d0ecc2805d0
szilardmolnar <szilard.molnar@icloud.com>
2025-06-01 20:45:30 +0200

    @NgModule

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,14 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root2',
   template: `<form #createForm="ngForm" (ngSubmit)="Update()"><button type=submit [disabled]="!createForm.form.valid">Change</button></form>`,
+})
+@NgModule({
+  imports: [
+             FormsModule
+           ],
 })
 export class AppComponent  {
   Update()
