77fc7b5c97e694bd31c282c6962b7971d3e1c110
szilardmolnar <szilard.molnar@icloud.com>
2025-06-05 17:36:30 +0200

    Node.js  throw err

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
+  selector: '',
   template: '<form (keydown)="keyDownFunction($event)"> </form>',
 })
 export class AppComponent {
---
+++ src/app/app.module.ts
@@ -0,0 +1,8 @@
+import { NgModule } from '@angular/core';
+
+@NgModule({
+  imports:      [ BrowserModule ],
+  declarations: [],
+  bootstrap:    [ AppComponent ]
+})
+export class AppModule {}
